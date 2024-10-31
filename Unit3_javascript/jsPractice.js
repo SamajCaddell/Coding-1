2f4d378620ba1c31bbcbeb1b8f3d7b07c6c7be2a
SamajCaddell <samajcaddell45@gmail.com>
2024-10-31 19:17:15 +0000

    jsPractive

---
+++ Unit3_javascript/jsPractice.js
@@ -0,0 +1,32 @@
+//Data types = most fundemental building block
+// of JS programming language.
+
+// String data type- are character bound together
+// With quotation marks
+"anything wrapped in quotes"
+"it can be numbers letters and symbol"
+"we can create strings with the double quotation marks"
+"we cam also write strings with single quotation marks."
+
+'yes id like to make 85,000-104,000 a year starting...'
+850000 - 104000
+
+// interger data type- numbers
+// doesnt matter if its a whole or decimal number 
+14313414314
+7656
+354.8775875
+0.8758
+
+//boolean data type-true or false value.
+true
+false
+
+// variables - containers for data
+// we create variabnles ny doing the following:
+//1. we need to declare the type of vatriable we are using.
+//2 we need to give the variable a name
+//3. we need to give it a equal sign.
+//4. we need ti give it data/ value.
+
+var weather_city = 'philly'

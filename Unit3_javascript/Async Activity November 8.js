42c1ac6bf9fc3111c5ffeae1c89b55f386b35549
SamajCaddell <samajcaddell45@gmail.com>
2024-11-12 20:17:23 +0000

    ReviewedAsyncAtivityNov8

--- Unit3_javascript/Async Activity November 8.js
+++ Unit3_javascript/Async Activity November 8.js
@@ -1,6 +1,16 @@
-var x = 12
-var y = 2
-var z = 12/4  // z equals 3
-x <= 12   // true (12 is equal to 12)
-z < 10    // true (3 is less than 10)
-console.log(x <= 12 && z < 10); 
+'Start with a Letter or Underscore: Variable names must begin with a letter (a-z, A-Z) or an underscore (_). They cannot start with a number.'
+'No Special Characters: Variable names can include letters, numbers, and underscores, but cannot contain spaces or special characters.'
+'Case Sensitivity: Variable names are case-sensitive, meaning myVariable, MyVariable, and MYVARIABLE are considered different variables.'
+'Variable Name Formats.'
+'Camel Case: This format capitalizes the first letter of each word except the first one, with no spaces or underscores.'
+'Snake Case: This format uses lowercase letters and underscores to separate words.'
+'Pascal Case: Similar to camel case, but the first letter of every word is capitalized, including the first one.'
+
+let a = 100; // Assigning 100 to a
+let b = a * 7; // Now b will be 700
+console.log(b); // Output: 700
+
+var x = 12;
+var y = 2;
+var z = 12 / 4;
+if (x <= 12 && z < 10)

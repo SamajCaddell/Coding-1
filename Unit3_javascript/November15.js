03af688d89c32127285b89399188331c098475d9
SamajCaddell <samajcaddell45@gmail.com>
2024-11-18 18:57:51 +0000

    November15AsyncActivity

--- Unit3_javascript/November15.js
+++ Unit3_javascript/November15.js
@@ -1,7 +1,20 @@
-The equal sign is used as an assignment operator in programming, assigning the value on the right to the variable on the left (e.g., x = 5). It also denotes equality in mathematics.
-2. Logical AND (&&)
-The double ampersand (&&) is a logical AND operator that returns true only if both boolean expressions are true (e.g., if (a > 0 && b > 0)).
-3. Logical OR (||)
-The double vertical bar (||) is a logical OR operator that returns true if at least one of the boolean expressions is true (e.g., if (a > 0 || b > 0)).
+var username = ''
+var password = ''
+"logical operater"
 
-The double equals sign (==) is a comparison operator that checks if two values are equal.
+comparison
+var ecgOrderRequest = 3
+var ecgShipmentRecieved =
+
+    'assignment operator'
+
+var messageToSend = hello class
+    varMessageToBeRec = messageToSend
+
+ single equal sign: assignment operator
+
+ double ampersand: logical AND operator
+
+ double vertical slash: logical OR operator
+
+ Double equal: comparison SAME / EQUAL

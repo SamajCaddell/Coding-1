ca81dda8d63c68e9cce15f006b9fd9f93254b325
SamajCaddell <samajcaddell45@gmail.com>
2024-11-19 20:13:34 +0000

    worked on fuctions

---
+++ Unit3_javascript/FuctionPractice/fuctions.js
@@ -0,0 +1,26 @@
+// What is fuction?
+'a fuction is a set instructions that preforms a specific task or job'
+
+//Fuctions come in 2 variations:
+// built in fuctions
+// into the coding lauguage, All we need to do is pass in data
+
+// examples
+var className= 'codingclass'
+console.log('Coding class.')
+
+// user-defined fuctions- These are custom instructions written by 
+// engineers to make code that preforms specific tasks.
+
+// how do we write a user-defined fuction?
+
+function exampleFuction(){
+    console.log('the fuction script is connected.')
+}
+
+// in order to have a fuction work, we need to call it.
+// the above is the fuction definition- Meaning- its only
+// telling the computer the instruction- NOT to actully run it.
+
+// we access or call a fuction by writing its name.
+exampleFuction()

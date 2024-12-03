49f48cf64e1956d748e7699c1bcccac39f97f8be
SamajCaddell <samajcaddell45@gmail.com>
2024-12-03 20:18:37 +0000

    handsOnPractice

---
+++ Unit3_javascript/handsOnFuction.js
@@ -0,0 +1,54 @@
+// Anatomy of a fuction
+
+// Curly brackets = this is
+// called a parameter = this is
+// inside of the fuctions parameters
+
+//parameter = placeholder for data
+
+//data = just means data types
+
+function pizzaParty(name, topping) {
+    console.log('congrats  ' + name + "you have been invited to the pizza party")
+    console.log("your custom pizza will have " + topping + "on it")
+}
+
+pizzaParty('MIKE' , 'veggies')
+
+// conditional statements in Js
+
+//conditional statements are written using
+// the 'IF' and 'ELSE' keywords This allows
+//use to make decisions based on data
+// our fuction rec
+
+function pizzaPartyInvitation(name) {
+    let guess = "mike";
+    if (name !=guest) {
+        console.log("this name is not on the list");
+    } else {
+        console.log("welcome to the party");
+    }
+}
+pizzaPartyInvitation('Ian')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Activity
+
+//1.Create a fuction that will convert a number

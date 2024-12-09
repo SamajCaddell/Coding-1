1f39e0a6173675531af33a21b81cc4cd0387f7ca
SamajCaddell <samajcaddell45@gmail.com>
2024-12-09 19:13:04 +0000

    LandMarkFunctionAsync

---
+++ Unit3_javascript/asyncActivityDec6.js
@@ -0,0 +1,11 @@
+function statedLandmark(state) {
+    if (state == "New york") {
+        console.log(" Statue of Liberty");
+    } else if (state == "Maine") {
+        console.log("Acadia National Park");
+    }
+    else {
+        console.log("the state is not listed");
+    }
+}
+statedLandmark('Washington') 

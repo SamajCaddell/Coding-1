6f7b3d3ccc951ec42e5a21a482eaf0627fa484fa
SamajCaddell <samajcaddell45@gmail.com>
2025-01-08 20:39:09 +0000

    CodingQuiz 1,2

---
+++ Unit3_javascript/Coding I and II Quarter 2 Quiz.js
@@ -0,0 +1,8 @@
+// 1.Interger data types are used to store a whole number without decimals.
+// 2.when you write a code else/if makes to code do some as "if this" or "else" to make it do something different.
+// 3.A "Function" is a block of code that has the abillity to go through with a task.
+// 4.A"Boolean Data Type" is a data type where when you put it down it puts a option of yes and no and/or on and off etc.
+// 5.An argrument is the value that are sent to the function when it gts called while, a parameter is the variable listed inside parentheses in the function definition.
+// 6.The "=="(equal to sign)
+// 7.The">="would be used to and for height and requirements of the height limit.
+// 8.

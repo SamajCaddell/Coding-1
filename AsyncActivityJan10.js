bf71eb3afe65b59380a2ae6a4a7d527ebe90e764
SamajCaddell <samajcaddell45@gmail.com>
2025-01-10 20:05:49 +0000

    AsyncActivityJan10

---
+++ AsyncActivityJan10.js
@@ -0,0 +1,42 @@
+//Create a list that includes 5 items that you can find in the frozen food isle.
+
+
+
+var groceryFozenFoodList = ['hotpocket','frozen fruits','pizza roll','frozen oven chicken','frozen pizza']
+var  RandomData = [' Yasir', 202020,
+   202.323,
+   'coding class',
+   true,
+   false,
+groceryFozenFoodList
+]
+
+
+
+
+//Create a list that includes 5 items that you can find in the international food isle.
+
+
+
+
+var groceryInternationalFoodList = ['seasoning','rice','soups','tea packets','baked beans']
+var  RandomData = [' Yasir', 202020,
+   202.323,
+   'coding class',
+   true,
+   false,
+groceryInternationalFoodList
+]
+
+
+//Creat a list that includes 5 items that you can find in the produce isle.
+
+
+var groceryProduceIsleList = ['keel','apples','oranges','peppers', 'mushrooms',]
+var  RandomData = [' Yasir', 202020,
+   202.323,
+   'coding class',
+   true,
+   false,
+groceryProduceIsleList
+]

5a74e72642c729410e788c6e0d43b709ba2add44
SamajCaddell <samajcaddell45@gmail.com>
2025-01-15 20:19:17 +0000

    ArrayMethodNotes

---
+++ jsArrayMethod.js
@@ -0,0 +1,42 @@
+// Javascript Lists are called arrays.
+
+// they are more flexible compared to other programing
+// language list data types.
+
+
+
+
+// in JS its universal.
+
+// JS array is a data type for collecting and storing
+// other data types, including other lists.
+
+var name1 = "Samaj":
+var grocery = [apple, milk, water"];
+
+// An array can store multiple data types
+var generalists = [name1, grocery, 132323, 23.34, true];
+
+// Array Mehthods
+// A method a function that works on a list(or object).
+// methods are written after the list variable name and
+// and declared witha period
+// pretty much; the method is going inside of the list
+// and running instructions on data from the list
+console.log()
+console.error()
+
+// Push method() - this method(special function)
+// adds a new place of data toa list. it will add that
+// new data to the back of the list
+generalists.push('new data')
+console.log(generalists)
+
+//pop method() - this method removes a piece of data
+// from a list. it will remove the last item in that list.
+generalists.pop()
+console.log(generalists)
+
+//Length() method - This method counts the number of data
+//Inside of a list
+console.log(generalists.length)

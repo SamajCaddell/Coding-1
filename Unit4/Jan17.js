53b33763c557eddea416137eee30bdf87982f1a2
SamajCaddell <samajcaddell45@gmail.com>
2025-01-21 20:18:39 +0000

    review

---
+++ Unit4/Jan17.js
@@ -0,0 +1,11 @@
+// 1. reverse order of a array
+campingSupplies = ['tent', 'sleeping bag', 'flash light', 'camping knife']
+
+const reversedcampingList = campingSupplies.reversed();
+console.log(reversedcampingList);
+
+// 2. add a new element to camping array
+reversedcampingList.push('water bottle')
+console.log(reversedcampingList)
+
+//3.combine the array above with the array below:
